7aa4b51a71668f05a613382e44e55ddf3a3f127a
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-04-08 14:36:41 +1000

    feat(exception-service): add http configurator

--- src/services/exception-service.ts
+++ src/services/exception-service.ts
@@ -1,7 +1,12 @@
 import { Exception } from "../exceptions/exception";
 import { IApiException } from "../interfaces/i-api-exception";
+import { UnauthorizedException } from "../exceptions/unauthorized";
+import { ForbiddenException } from "../exceptions/forbidden";
+import { HttpClient } from "aurelia-fetch-client";
 
 export class ExceptionService {
+	constructor(private http: HttpClient) { }
+
 	registry = new Map<string, typeof Exception>();
 
 	register(className: string, exceptionType: typeof Exception) {
@@ -15,4 +20,26 @@ export class ExceptionService {
 		}
 		throw new exceptionType(apiException);
 	}
+
+	async assertResponse(r: Response): Promise<Response> {
+		if (r.ok) {
+			return r;
+		}
+		else if (r.status === 401) {
+			throw new UnauthorizedException();
+		}
+		else if (r.status === 403) {
+			throw new ForbiddenException();
+		}
+		else {
+			return r.json().then<any>((d: IApiException) => this.throw(d));
+		}
+	}
+
+	addHttpInterceptor() {
+		this.http.configure(c => {
+			c.withInterceptor({ response: r => this.assertResponse(r) });
+		});
+	}
+
 }
